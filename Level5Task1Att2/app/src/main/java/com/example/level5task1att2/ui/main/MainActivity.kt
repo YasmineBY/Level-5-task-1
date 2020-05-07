d5456873df866bda02904f88e37093b6d7d1cbb9
yasmine By <benyasminenl@gmail.com>
2020-05-07 20:18:08 +0200

    Finished

--- Level5Task1Att2/app/src/main/java/com/example/level5task1att2/ui/main/MainActivity.kt
+++ Level5Task1Att2/app/src/main/java/com/example/level5task1att2/ui/main/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.level5task1att2.ui.main
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -8,6 +9,7 @@ import android.view.MenuItem
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.level5task1att2.R
+import com.example.level5task1att2.ui.edit.EditActivity
 
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
@@ -27,11 +29,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initViews() {
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        fab.setOnClickListener {
+            val intent = Intent(this, EditActivity::class.java)
+            intent.putExtra(EditActivity.EXTRA_NOTE, mainActivityViewModel.note.value)
+            startActivity(intent)
         }
     }
+
 
     private fun initViewModel() {
         mainActivityViewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
