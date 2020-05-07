4a43135637a7f1b41137efda3562eae4377f1b4b
yasmine By <benyasminenl@gmail.com>
2020-05-07 04:05:07 +0200

    Working on layout

--- Level 5 - Task 1/app/src/main/java/com/example/level5task1/ui/edit/EditActivity.kt
+++ Level 5 - Task 1/app/src/main/java/com/example/level5task1/ui/edit/EditActivity.kt
@@ -1,12 +1,13 @@
-package com.example.level5task1
+package com.example.level5task1.ui.edit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.level5task1.R
 
-class MainActivity : AppCompatActivity() {
+class EditActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_edit)
     }
 }
